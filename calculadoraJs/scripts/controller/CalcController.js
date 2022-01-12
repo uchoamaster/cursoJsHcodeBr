d6f8cf9eb9d9cfc4c5b0ffafc8c6d3916c1a6f55
Carlos Uchoa <carlosjuchoa@hotmail.com>
2022-01-12 20:37:37 -0300

    finalizando bug do setinterval

--- calculadoraJs/scripts/controller/CalcController.js
+++ calculadoraJs/scripts/controller/CalcController.js
@@ -1,62 +1,85 @@
 class CalcController {
 
-        constructor(){
+    constructor() {
 
-            //poderia ter iniciado com var de variavel mas como iremos utilizar em todos os locais do aplicativo calculadora, é interessante utilizar o this, que ele manipula metodos e atributos 
-            //quando está com este _ underline na frente do atributo significa que ele está como private então não poderá ser acessado
-            //aqui consigo selecionar os campos selecionados pelos determinados id no caso id display, data e hora.
-            this._locale = 'pt-BR';
-            this._displayCalcEl =  document.querySelector("#display");
-            this._dateEl =  document.querySelector("#data");
-            this._timeEl = document.querySelector("#hora");
-            this._currentDate;
-            this.initialize();
-        }
-        //metodo que iremos utilizar para quando eu quiser que aconteça algo ele inicialize automaticamente ou com uma ação
-        initialize(){   
-            //aqui com este atributo innerHTML ele pede para por a informação dentro do html no campo que foi selecionado
-       
-        //    this._dateEl.innerHTML = ("12/01/2022");
-        //    this._timeEl.innerHTML = ("15:09");
-        setInterval(function(){
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
 
-            this.displayDate = this.currentDate.toLocaleDateString(this._locale);
-            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
 
-        }.bind(this),1000);
-        }
+    initialize() {
 
-//toda vez que eu criar um atributo privado seja no construtor ou em outro metodo, ele precisa de como será retirado e mostrado ou seja get e set dele senão nao funciona
-        get displayTime(){
-            return this._timeEl.innerHTML;
-        }
+        this.setDisplayDateTime()
 
-        set displayTime(value){
-             this._timeEl.innerHTML = value;
-        }
-        
-        get displayDate(){
-            return this._dateEl.innerHTML;
-        }
+        setInterval(() => {
 
-        set displayDate(value){
-             this._dateEl.innerHTML = value;
-        }
+            this.setDisplayDateTime();
 
-        get displayCalc(){
-            return this._displayCalcEl.innerHTML;
-        }
+        }, 1000);
 
-        set displayCalc(value){
-            this._displayCalcEl.innerHTML = value;
-        }
+    }
 
-        get currentDate(){
-            return this._currentDate;
-        }
+    setDisplayDateTime() {
 
-        set currentDate(value){
-            this._currentDate = value;
-        }
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
+    }
+
+    get displayTime() {
+
+        return this._timeEl.innerHTML;
+
+    }
+
+    set displayTime(value) {
+
+        return this._timeEl.innerHTML = value;
+
+    }
+
+    get displayDate() {
+
+        return this._dateEl.innerHTML;
+
+    }
+
+    set displayDate(value) {
+
+        return this._dateEl.innerHTML = value;
+
+    }
+
+    get displayCalc() {
+
+        return this._displayCalcEl.innerHTML;
+
+    }
+
+    set displayCalc(value) {
+
+        this._displayCalcEl.innerHTML = value;
+
+    }
+
+    get currentDate() {
+
+        return new Date();
+
+    }
+
+    set currentDate(value) {
+
+        this._currentDate = value;
+
+    }
 
 }
