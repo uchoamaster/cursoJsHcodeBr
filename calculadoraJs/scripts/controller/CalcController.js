ef9f63dbd1ab55abb352b2b75427c81d00ad0aef
Carlos Uchoa <carlosjuchoa@hotmail.com>
2022-01-12 10:43:14 -0300

    criando get e set do metodo privado criado

--- calculadoraJs/scripts/controller/CalcController.js
+++ calculadoraJs/scripts/controller/CalcController.js
@@ -1,5 +1,26 @@
 class CalcController {
 
-    
+        constructor(){
+            //poderia ter iniciado com var de variavel mas como iremos utilizar em todos os locais do aplicativo calculadora, é interessante utilizar o this, que ele manipula metodos e atributos 
+            //quando está com este _ underline na frente do atributo significa que ele está como private então não poderá ser acessado
+            this._displayCalc = "0";
+            this._dataAtual;
+        }
+//toda vez que eu criar um atributo privado seja no construtor ou em outro metodo, ele precisa de como será retirado e mostrado ou seja get e set dele senão nao funciona
+        get displayCalc(){
+            return this._displayCalc;
+        }
+
+        set displayCalc(valor){
+            this._displayCalc = valor;
+        }
+
+        get dataAtual(){
+            return this._dataAtual;
+        }
+
+        set dataAtual(valor){
+            this._dataAtual = valor;
+        }
 
 }
