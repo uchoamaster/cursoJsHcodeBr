e6b9c00e75e18117a9c03e57019acd9bfbc64018
Carlos Uchoa <carlosjuchoa@hotmail.com>
2022-01-12 17:00:20 -0300

    aplicando locale

--- calculadoraJs/scripts/calculator.js
+++ calculadoraJs/scripts/calculator.js
@@ -1,2 +1,2 @@
 //aqui estou dizendo que esta variavel calculadora é igual a uma nova instância ou nova copia da classe CalcController
-let calculator = new CalcController();
+window.calculator = new CalcController();
--- calculadoraJs/scripts/controller/CalcController.js
+++ calculadoraJs/scripts/controller/CalcController.js
@@ -1,39 +1,62 @@
 class CalcController {
 
         constructor(){
+
             //poderia ter iniciado com var de variavel mas como iremos utilizar em todos os locais do aplicativo calculadora, é interessante utilizar o this, que ele manipula metodos e atributos 
             //quando está com este _ underline na frente do atributo significa que ele está como private então não poderá ser acessado
-            this._displayCalc = "0";
+            //aqui consigo selecionar os campos selecionados pelos determinados id no caso id display, data e hora.
+            this._locale = 'pt-BR';
+            this._displayCalcEl =  document.querySelector("#display");
+            this._dateEl =  document.querySelector("#data");
+            this._timeEl = document.querySelector("#hora");
             this._currentDate;
             this.initialize();
         }
         //metodo que iremos utilizar para quando eu quiser que aconteça algo ele inicialize automaticamente ou com uma ação
         initialize(){   
-            //aqui consigo selecionar os campos selecionados pelos determinados id no caso id display, data e hora.
-           let displayCalEl =  document.querySelector("#display");
-           let dateEl =  document.querySelector("#data");
-           let timeEl = document.querySelector("#hora");
             //aqui com este atributo innerHTML ele pede para por a informação dentro do html no campo que foi selecionado
-           displayCalEl.innerHTML = ("4567");
-           dateEl.innerHTML = ("12/01/2022");
-           timeEl.innerHTML = ("15:09");
+       
+        //    this._dateEl.innerHTML = ("12/01/2022");
+        //    this._timeEl.innerHTML = ("15:09");
+        setInterval(function(){
+
+            this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
+        }.bind(this),1000);
         }
 
 //toda vez que eu criar um atributo privado seja no construtor ou em outro metodo, ele precisa de como será retirado e mostrado ou seja get e set dele senão nao funciona
-        get displayCalc(){
-            return this._displayCalc;
+        get displayTime(){
+            return this._timeEl.innerHTML;
         }
 
-        set displayCalc(valor){
-            this._displayCalc = valor;
+        set displayTime(value){
+             this._timeEl.innerHTML = value;
+        }
+        
+        get displayDate(){
+            return this._dateEl.innerHTML;
+        }
+
+        set displayDate(value){
+             this._dateEl.innerHTML = value;
+        }
+
+        get displayCalc(){
+            return this._displayCalcEl.innerHTML;
+        }
+
+        set displayCalc(value){
+            this._displayCalcEl.innerHTML = value;
         }
 
         get currentDate(){
             return this._currentDate;
         }
 
-        set currentDate(valor){
-            this._currentDate = valor;
+        set currentDate(value){
+            this._currentDate = value;
         }
 
 }
