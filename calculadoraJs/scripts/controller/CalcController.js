b3237ae720cc3a73cc8853919c9b15720cd0beba
Carlos Uchoa <carlosjuchoa@hotmail.com>
2022-01-13 11:55:07 -0300

    adicionando evento de click e aprendendo sobre regras e condições

--- calculadoraJs/scripts/controller/CalcController.js
+++ calculadoraJs/scripts/controller/CalcController.js
@@ -8,6 +8,7 @@ class CalcController {
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
 
     }
 
@@ -23,10 +24,18 @@ class CalcController {
 
     }
 
-    initButtonsEvents(){
+    initButtonsEvents() {
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
+        buttons.forEach((btn, index) => {
+
+            btn.addEventListener('click', e => {
+
+                console.log(btn.className.baseVal.replace("btn-",""));
+            });
+    
+        })
 
     }
 
