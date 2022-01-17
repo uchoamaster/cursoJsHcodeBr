fa3a1af802ff024a168cddee7283d4cd2cd02e15
Carlos Uchoa <carlosjuchoa@hotmail.com>
2022-01-17 11:13:17 -0300

    utilizando o for e aprendendo sobre for

--- calculadoraJs/scripts/controller/CalcController.js
+++ calculadoraJs/scripts/controller/CalcController.js
@@ -56,12 +56,21 @@ class CalcController {
         let last = this._operation.pop();
         let result = eval(this._operation.join(""));
         this._operation = [result, last];
-        console.log(this._operation);
+        // console.log(this._operation);
+        this.setLastNumberToDisplay();
     }
 
     setLastNumberToDisplay(){
 
-        
+        let lastNumber;
+    for(let i = this._operation.length -1; i >= 0; i--){
+        if(!this.isOperator(this._operation[i])) {
+            lastNumber = this._operation[i];
+            break;
+        }
+    }
+    this.displayCalc = lastNumber;       
+
 
     }
     addOperation(value){
@@ -80,6 +89,7 @@ class CalcController {
         }else{
 
             this.pushOperation(value);
+            this.setLastNumberToDisplay();
         }
         
         
