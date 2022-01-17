67a6fa0f1778be7a3c3aa9380e4fdb2af08015ca
Carlos Uchoa <carlosjuchoa@hotmail.com>
2022-01-17 17:01:23 -0300

    colocando som no teclado na tecla ac

--- calculadoraJs/scripts/controller/CalcController.js
+++ calculadoraJs/scripts/controller/CalcController.js
@@ -2,6 +2,8 @@ class CalcController {
 
     constructor(){
 
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
 
@@ -29,6 +31,7 @@ class CalcController {
         input.value = this.displayCalc;
         document.body.appendChild(input);
         input.select();
+        //codigo depreciado, verificar depois alternativa
         document.execCommand("Copy");
         input.remove();
     }
@@ -47,10 +50,35 @@ class CalcController {
         this.setLastNumberToDisplay();
         this.pastFromClipBoard();
 
+        document.querySelectorAll('.btn-ac').forEach(btn=>{
+            btn.addEventListener('dblclick',e=>{
+                this.toggleAudio();
+            });
+        });
+
+    }
+
+    toggleAudio(){
+
+        this._audioOnOff =  !this._audioOnOff;
+        // this._audioOnOff = (this._audioOnOff) ?  false : true;
+        // if(this._audioOnOff){
+        //     this._audioOnOff = false;
+        // }else {
+        //     this._audioOnOff = true;
+        // }
+    }
+
+    playAudio(){
+        if(this._audioOnOff){
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
     }
 
     initKeyboard(){
         document.addEventListener('keyup', e=>{
+            this.playAudio();
             // console.log(e.key);
             switch (e.key) {
 
@@ -308,6 +336,8 @@ class CalcController {
     }
 
     execBtn(value){
+
+        this.playAudio();
 
         switch (value) {
 
