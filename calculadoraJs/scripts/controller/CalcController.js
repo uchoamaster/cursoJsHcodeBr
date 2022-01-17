eb7495cad74bea22e5dace2ac1f052c6ffea11f3
Carlos Uchoa <carlosjuchoa@hotmail.com>
2022-01-17 15:13:30 -0300

    botão ponto

--- calculadoraJs/scripts/controller/CalcController.js
+++ calculadoraJs/scripts/controller/CalcController.js
@@ -43,6 +43,8 @@ class CalcController {
     clearAll(){
 
         this._operation = [];
+        this._lastNumber = '';
+        this._lastOperator = '';
 
         this.setLastNumberToDisplay();
 
@@ -207,7 +209,7 @@ class CalcController {
 
                 let newValue = this.getLastOperation().toString() + value.toString();
 
-                this.setLastOperation(parseInt(newValue));
+                this.setLastOperation(parseFloat(newValue));
 
                 this.setLastNumberToDisplay();
 
@@ -221,6 +223,18 @@ class CalcController {
 
         this.displayCalc = "Error";
         
+    }
+
+    addDot(){
+
+     let lastOperation = this.getLastOperation();  
+        // console.log(lastOperation);
+        if(this.isOperator(lastOperation) || !lastOperation){
+            this.pushOperation('0.');
+        }else{
+            this.setLastOperation(lastOperation.toString()+ '.');
+        }
+        this.setLastNumberToDisplay();
     }
 
     execBtn(value){
@@ -260,7 +274,7 @@ class CalcController {
                 break;
 
             case 'ponto':
-                this.addOperation('.');
+                this.addDot('.');
                 break;
 
             case '0':
